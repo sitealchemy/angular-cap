aa88a7260892c630bc61b5e4445a8cd84215bd71
Grzegorz Serwin <grzegorz.serwin@gmail.com>
2023-10-12 11:41:36 +0200

    init

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'itbeess.angcap.com',
+  appName: 'angular-cap',
+  webDir: 'dist/angular-cap',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
